b0f00f1e46946061a0c9006551e8f113994bc795
Rohit Suri <rohit.suri@aethereus.com>
2020-12-15 23:03:59 +0530

    first commit

---
+++ credentials.js
@@ -0,0 +1,16 @@
+module.exports = {
+  salesforce: {
+    consumer_key:
+      '3MVG97quAmFZJfVy_AzTVls98LpU2h6i7mGC7GHXbPV.sLTSbck1dSXZ1S0gw9N1szLqEIOmnFAc3D2AWyTtb',
+    consumer_secret:
+      '65B1719692EA644043D1D3197D8FAFD28DB1C4D9DB4CCDC05881544971C06AA2',
+    url: 'https://login.salesforce.com',
+    username: 'rohit.suri@flow.com',
+  },
+  google: {
+    consumer_key: '',
+    consumer_secret: '',
+    callback_url: 'https://login.salesforce.com/services/oauth2/success',
+  },
+  session: { secret: 'keyboard cat' },
+}
